a9dea40d7bab07ba4966d9f33466fcecacb48bf6
MichalLastovka <112790340+MichalLastovka@users.noreply.github.com>
2023-03-03 09:44:41 +0100

    Create indoor.py

---
+++ Week 1 (Functions, Variables)/indoor.py
@@ -0,0 +1,6 @@
+# Task:
+# In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase. 
+# Punctuation and whitespace should be outputted unchanged.
+
+user_text_input = str(input("Enter the text: "))
+print((user_text_input).lower())
