c14a8c3eb3aa2070c0498b76625f0f401ccebb7d
MichalLastovka <112790340+MichalLastovka@users.noreply.github.com>
2023-05-09 21:24:31 +0200

    Create response.py

---
+++ Week 9 (Regular expressions)/response.py
@@ -0,0 +1,20 @@
+"""
+Task:
+
+In a file called response.py, using either validator-collection or validators from PyPI, implement a program that prompts the user for an email address via input and then prints Valid or Invalid, respectively, 
+if the input is a syntatically valid email address. You may not use re. And do not validate whether the email address’s domain name actually exists.
+
+"""
+
+from validator_collection import checkers
+
+def main():
+    email = str(input("What's your e-mail address? "))
+    if checkers.is_email(email):
+        return print("Valid")
+    else:
+        return print("Invalid")
+
+
+if __name__ == "__main__":
+    main()
